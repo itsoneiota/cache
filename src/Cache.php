06341f3a41fe3d934c658f39e749b5332d5dc7bf
rossmcf <ross@rossmcf.com>
2015-11-06 11:03:33 +0000

    Circumvent increment with initial value bug in PHP Memcached.
    Address a bug in the PHP Memcached library where keys incremented with
    an initial value can return junk data.

--- src/Cache.php
+++ src/Cache.php
@@ -174,6 +174,25 @@ class Cache {
 		return $this->cache->set($this->mapKey($key), $this->mapValue($value), $this->mapExpiration($expiration));
 	}
 
+	protected $incrementDecrementKeys = [];
+	/**
+	 * Initialise a key for use with `increment()` and `decrement()`.
+	 *
+	 * @param string $key
+	 * @param int $initialValue
+	 */
+	protected function initialiseIncrementDecrementKey($key, $initialValue, $expiry){
+		if (!is_int($initialValue)) {
+			throw new \InvalidArgumentException('Initial value must be an integer.');
+		}
+		$added = FALSE;
+		if (!in_array($key, $this->incrementDecrementKeys)) {
+			$added = $this->add($key, $initialValue, $expiry);
+			$this->incrementDecrementKeys[] = $key;
+		}
+		return $added;
+	}
+
 	/**
 	 * Increment a numeric item's value by the specified offset.
 	 * If the item's value is not numeric, an error will result.
@@ -185,6 +204,27 @@ class Cache {
 	 * @return boolean TRUE on success or FALSE on failure.
 	 */
 	public function increment($key, $offset=1, $initialValue=0, $expiry=NULL){
+		/**
+		 * Due to a bug in the PHP Memcached client, keys incremented
+		 * with an initial value can return junk data in their results.
+		 *
+		 * The only real way around this is to ensure that the key exists,
+		 * and set its initial value before incrementing.
+		 *
+		 * "Memcached::increment() will set the item to the initial_value parameter if the key doesn't exist."
+		 * With this in mind, if we always tried to `add()` _and_ `increment()`,
+		 * we'd end up with a value of initial + increment if the key didn't exist.
+		 * So, we need to try adding the key, then only increment if it didn't previously exist.
+		 *
+		 * @see http://stackoverflow.com/questions/33550880/php-memcached-with-binary-protocol-junk-data-returned-after-increment
+		 */
+		$keyAdded = $this->initialiseIncrementDecrementKey($key, $initialValue, $expiry);
+		if ($keyAdded) {
+			return TRUE;
+		}
+		/**
+		 * Unfortunately, we still need to set initialValue on increment, so that we can supply the expiry.
+		 */
 		return $this->cache->increment($this->mapKey($key), $offset, $initialValue, $this->mapExpiration($expiry));
 	}
 
@@ -200,6 +240,27 @@ class Cache {
 	 * @return boolean TRUE on success or FALSE on failure.
 	 */
 	public function decrement($key, $offset=1, $initialValue=0, $expiry=NULL){
+		/**
+		 * Due to a bug in the PHP Memcached client, keys decremented
+		 * with an initial value can return junk data in their results.
+		 *
+		 * The only real way around this is to ensure that the key exists,
+		 * and set its initial value before decrementing.
+		 *
+		 * "Memcached::decrement() will set the item to the initial_value parameter if the key doesn't exist."
+		 * With this in mind, if we always tried to `add()` _and_ `decrement()`,
+		 * we'd end up with a value of initial - decrement if the key didn't exist.
+		 * So, we need to try adding the key, then only decrement if it didn't previously exist.
+		 *
+		 * @see http://stackoverflow.com/questions/33550880/php-memcached-with-binary-protocol-junk-data-returned-after-increment
+		 */
+		$keyAdded = $this->initialiseIncrementDecrementKey($key, $initialValue, $expiry);
+ 		if ($keyAdded) {
+ 			return TRUE;
+ 		}
+		/**
+		 * Unfortunately, we still need to set initialValue on decrement, so that we can supply the expiry.
+		 */
 		return $this->cache->decrement($this->mapKey($key), $offset, $initialValue, $this->mapExpiration($expiry));
 	}
 
